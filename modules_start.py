4f93f7b39586ed1ad5ad7d387b83f07214623844
Cody Fenn <89817722+codyfenn@users.noreply.github.com>
2022-07-18 14:04:34 -0400

    Add files via upload

---
+++ modules_start.py
@@ -0,0 +1,12 @@
+# import the math module, which contains features for working with mathematics
+import math
+
+# the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants
+print("Pi is:", math.pi)
+
+# try some of the math functions for yourself here:
+print("Sin 25 is:", math.sin(25) )
+print("Sin 1 is:", math.tan(1) )
